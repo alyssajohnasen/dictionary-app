35712ab312951b857c1fc4b41826eb837bf92449
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-06-29 20:54:20 -1000

    Looped through each meaning & created Meanings component to store each meaning & definition

--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Meanings from "./Meanings";
 
 export default function Results(props) {
-  console.log(props.results);
   if (props.results) {
-    return ( <div className="Results">
+    return ( 
+    <div className="Results">
       <h2>{props.results.word}</h2>
+      {props.results.meanings.map(function (meaning, index) {
+        return (
+          <div key={index}>
+            <Meanings meanings={meaning} />
+          </div>
+        )
+      })}
     </div>
     );
   } else
