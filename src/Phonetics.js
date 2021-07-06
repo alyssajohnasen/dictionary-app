299f86908724f5a6266cabeef435a783fbc3d56c
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-07-05 21:01:15 -1000

    Added listen icon to phonetics

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,7 +4,7 @@ import "./Phonetics.css";
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">Listen</a>
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer"><i class="fas fa-volume-up"></i></a>
       <span className="text">{props.phonetics.text}</span>
     </div>
   )
