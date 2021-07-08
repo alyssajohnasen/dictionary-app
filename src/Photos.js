872e46383bd37ff3e86828a7b74455a13ec929aa
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-07-07 21:11:12 -1000

    Styled photos with responsive grid & displayed 9 photos per word definition

--- src/Photos.js
+++ src/Photos.js
@@ -1,15 +1,21 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
     return (
       <section className="Photos">
-        {props.photos.map(function(photo, index) {
-          return <div key={index} >
-            <img src={photo.src.tiny} alt={photo.photographer} />
-          </div>
-        })}
+        <div className="row">
+          {props.photos.map(function(photo, index) {
+            return ( 
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} alt={photo.photographer} className="img-fluid" />
+                </a>
+              </div>
+            ); 
+          })}
+        </div> 
       </section>
     );
   } else {
