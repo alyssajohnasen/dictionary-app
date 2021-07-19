9f52d345980b6210d390af15092a0e3f3b710110
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-07-18 18:29:45 -1000

    Added onClick phonetics audio feature

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,9 +2,14 @@ import React from "react";
 import "./Phonetics.css";
 
 export default function Phonetics(props) {
+  const audio = new Audio(props.phonetics.audio);
+  const playSound = (audioFile) => {
+    audioFile.play();
+  };
+
   return (
     <div className="Phonetics">
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer"><i className="fas fa-volume-up"></i></a>
+      <i className="fas fa-volume-up" onClick={() => playSound(audio)}></i>
       <span className="text">{props.phonetics.text}</span>
     </div>
   )
